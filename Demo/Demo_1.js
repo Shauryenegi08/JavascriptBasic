aecb16f3877dddc1799e15a6404ad4a62e29717a
Shauryenegi08 <negishaurye@gmail.com>
2025-07-04 12:57:26 +0530

    created develop branches

---
+++ Demo/Demo_1.js
@@ -0,0 +1,9 @@
+console.log ("initializing Demo_1.js");
+// if-else statement
+// let a = 10;
+// if (a > 5) {
+//     console.log("a is greater than 5");
+// }
+// else {
+//     console.log("a is not greater than 5");
+// }
